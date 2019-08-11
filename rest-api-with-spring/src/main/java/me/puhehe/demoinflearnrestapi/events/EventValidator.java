16643a821e79333ca05a0f91f4742e01374c260c
minhyungPark <puhehe99@gmail.com>
2019-08-11 20:34:17 +0900

    Body for BadRequest

--- rest-api-with-spring/src/main/java/me/puhehe/demoinflearnrestapi/events/EventValidator.java
+++ rest-api-with-spring/src/main/java/me/puhehe/demoinflearnrestapi/events/EventValidator.java
@@ -10,14 +10,15 @@ public class EventValidator {
 
     public void validate(EventDto eventDto, Errors errors) {
         if (eventDto.getBasePrice() >eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
-            errors.rejectValue("basePrice","wrongValue","BasePrice is wrong");
-            errors.rejectValue("maxPrice","wrongValue","MaxPrice is wrong");
+            // Global Error 에 들어간다.
+            errors.reject("wrongPrices","Values of prices are wrong");
         }
 
         LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
         if (endEventDateTime.isBefore(eventDto.getBeginEventDateTime()) ||
         endEventDateTime.isBefore(eventDto.getCloseEnrollmentDateTime()) ||
         endEventDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
+            // field error 에 들어간ㄷ.
             errors.rejectValue("endEventDateTime","wrongValue","endEventDateTime is wrong");
         }
 
