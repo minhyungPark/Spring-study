74d1da851b40509c8e3d934f2e69ca5d45f0dfb9
minhyungPark <puhehe99@gmail.com>
2019-08-13 19:36:09 +0900

    Apply Spring HATEOAS

--- rest-api-with-spring/src/main/java/me/puhehe/demoinflearnrestapi/events/EventController.java
+++ rest-api-with-spring/src/main/java/me/puhehe/demoinflearnrestapi/events/EventController.java
@@ -1,7 +1,9 @@
 package me.puhehe.demoinflearnrestapi.events;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -44,7 +46,11 @@ public class EventController {
         Event event = modelMapper.map(eventDto,Event.class);
         event.update();
         Event newEvent = this.eventRepository.save(event);
-        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
-        return ResponseEntity.created(createdUri).body(event);
+        ControllerLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        URI createdUri = selfLinkBuilder.toUri();
+        EventResource eventResource = new EventResource(event);
+        eventResource.add(linkTo(EventController.class).withRel("query-events"));
+        eventResource.add(selfLinkBuilder.withRel("update-event"));
+        return ResponseEntity.created(createdUri).body(eventResource);
     }
 }
