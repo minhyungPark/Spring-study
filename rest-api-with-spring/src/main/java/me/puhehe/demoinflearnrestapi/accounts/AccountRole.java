e2c497c50c58ac5a874300bcccbe6567e84e4414
minhyungPark <puhehe99@gmail.com>
2019-08-26 14:49:00 +0900

    Add Account domain

---
+++ rest-api-with-spring/src/main/java/me/puhehe/demoinflearnrestapi/accounts/AccountRole.java
@@ -0,0 +1,6 @@
+package me.puhehe.demoinflearnrestapi.accounts;
+
+public enum AccountRole {
+
+    ADMIN, USER
+}
--- rest-api-with-spring/src/main/java/me/puhehe/demoinflearnrestapi/events/Event.java
+++ rest-api-with-spring/src/main/java/me/puhehe/demoinflearnrestapi/events/Event.java
@@ -1,6 +1,7 @@
 package me.puhehe.demoinflearnrestapi.events;
 
 import lombok.*;
+import me.puhehe.demoinflearnrestapi.accounts.Account;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -26,6 +27,8 @@ public class Event {
     private boolean free;
     @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
+    @ManyToOne
+    private Account manager;
 
     public void update() {
         if(this.basePrice == 0 && this.maxPrice == 0 ){
