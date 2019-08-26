a396930cb874f0d9305293390f215942a5a6663d
minhyungPark <puhehe99@gmail.com>
2019-08-26 14:39:59 +0900

    Refactoring test codes

---
+++ rest-api-with-spring/src/test/java/me/puhehe/demoinflearnrestapi/common/BaseControllerTest.java
@@ -0,0 +1,34 @@
+package me.puhehe.demoinflearnrestapi.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
+@Ignore     // 테스트를 가지고 있지 않는 클래스라는 것을 알려주는 annotation
+public class BaseControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected ModelMapper modelMapper;
+
+}
